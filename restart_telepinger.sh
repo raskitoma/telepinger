6f15bb8d30321b7daa6108e8fed2fe8e64df3516
Elias Achi <elias@plazacybersecurity.com>
2023-04-11 22:25:17 -0500

    fix restart

--- restart_telepinger.sh
+++ restart_telepinger.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Restart telepinger
 
-docker compose down && git pull && docker compose up -d && docker compose logs -f
+docker compose down && git pull && docker compose build && docker compose up -d && docker compose logs -f
