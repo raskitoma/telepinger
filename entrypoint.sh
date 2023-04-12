6646cf95351357767b9d31187ace1846c993d770
Elias Achi <elias@plazacybersecurity.com>
2023-04-12 11:27:11 -0500

    Add more logs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+echo "Starting telepinger..."
+
+echo "Current environment variables:"
+
+env | grep MINUTES_
+env | grep PING_
+env | grep INFLUX_
+env | grep NOTIFY_
+
 # Set up the cron job
 echo "*/$MINUTES_INTERVAL * * * * /usr/local/bin/python /app/telepinger.py -c $PING_PACKETS -i $PING_INTERVAL $PING_HOST > /proc/1/fd/1 2>&1" | crontab -
 crontab -l
